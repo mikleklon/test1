00784954f4b03509bdb18482d4d8df131e61a191
root <root@localhost.localdomain>
2018-02-11 11:44:42 +0400

    start

---
+++ basic/views/site/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Start';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Hello!</h1>
+    </div>
+
+    <div class="body-content">
+
+
+    </div>
+</div>
